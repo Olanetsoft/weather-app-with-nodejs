5a58c236b1f298dd8d787abd3c0e9b37f7dcbb0c
olanetsoft <heedris2olubisi@gmail.com>
2020-08-21 16:36:26 +0100

    restructuring

--- app.js
+++ app.js
@@ -37,14 +37,15 @@ app.get('/', (req, res) => {
 app.post('/find', (req, res) => {
     const apiKey = process.env.API_KEY;
     const { location } = req.body;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&cnt=7&appid=${apiKey}`;
 
     request(url, function (err, response, body) {
         if (err) {
             console.log('error:', error);
         } else {
             let w = JSON.parse(body)
-            console.log('body:', w.main.temp - 273.15);
+            // console.log('body:', w.main.temp - 273.15);
+            console.log('body:', w.main.temp);
         }
     });
     res.redirect('/');
